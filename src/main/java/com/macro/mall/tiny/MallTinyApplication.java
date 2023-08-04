1356387b47f367a19c5799132316a4922c2a3996
jinxin <jinxin@hsmap.com>
2023-08-04 10:20:32 +0800

    chore: 配置修改

--- src/main/java/com/macro/mall/tiny/MallTinyApplication.java
+++ src/main/java/com/macro/mall/tiny/MallTinyApplication.java
@@ -1,9 +1,11 @@
 package com.macro.mall.tiny;
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@NacosPropertySource(dataId = "mall-tiny.yaml", groupId = "MALL_TINY", autoRefreshed = true)
 public class MallTinyApplication {
 
     public static void main(String[] args) {
